3a3cc10b7858aaa582dfddca7b7c22db3ce4d658
root <root@AeroCool.>
2024-12-01 21:02:11 +0100

    simple main menu and main window configuration - setting main (top) window in which game will be played, and second (bottom) window for additional information

--- frog.c
+++ frog.c
@@ -1,7 +1,152 @@
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <ncurses.h>
+
+
+#define BORDER		1
+#define DELAY_ON	1
+#define DELAY_OFF	0
+
+#define MAIN_COLOR      1
+#define STAT_COLOR      2
+#define PLAY_COLOR      3
+
+#define MAX_LVL 3
+
+#define ENTER 10
+// playwin (WIN) parameters
+#define ROWS		25							
+#define COLS		60
+#define OFFY		4
+#define OFFX		8
+
+///////////////////////////////////
+////////////Structs///////////////
+/////////////////////////////////
+
+// window structure
+typedef struct {
+	WINDOW* window;								
+	int x, y;
+	int rows, cols;
+	int color;
+} WIN;										
+
+//////////////////////////////////
+//////////////Functions//////////
+////////////////////////////////
+
+WINDOW* Start()
+{
+	WINDOW* win;
+	if ( (win = initscr()) == NULL ) {					// initialize ncurses
+		fprintf(stderr, "Error initialising ncurses.\n");
+		exit(EXIT_FAILURE);
+    	}
+
+	start_color();								// initialize colors
+	init_pair(MAIN_COLOR, COLOR_WHITE, COLOR_BLACK);
+	init_pair(PLAY_COLOR, COLOR_BLUE, COLOR_WHITE);
+	init_pair(STAT_COLOR, COLOR_WHITE, COLOR_BLUE);
+	
+	noecho();		//NIE wypisuje inputu na ekran 
+	curs_set(0);
+	return win;
+}
+
+void CleanWin(WIN* W, int bo)							// bo(rder): 0 | 1
+{
+	int i, j;
+	wattron(W->window,COLOR_PAIR(W->color));
+	if (bo) box(W->window,0,0);
+	for(i = bo; i < W->rows - bo; i++)
+		for(j = bo; j < W->cols - bo; j++)
+			mvwprintw(W->window,i,j," ");
+}
+void Welcome(WINDOW* win)	
+{
+	// screen with - press any key to continue
+	mvwaddstr(win, 1, 1, "Do you want to play a game?");
+	mvwaddstr(win, 2, 1, "Press any key to continue..");
+	wgetch(win);
+	// clear next refresh)								
+	wclear(win);								
+	wrefresh(win);
+
+}
+
+void Menu(WIN* win)
+{
+	char choices[3][20] = {"level 1","level 2","level 3"};
+
+	int move;
+	int current = 0;
+
+	while(1)
+	{		
+		for(int i = 0; i < MAX_LVL; i++)
+		{
+			if(i == current)
+				wattron(win->window,A_REVERSE);
+			mvwprintw(win->window,i+1,1,choices[i]);
+			wattroff(win->window,A_REVERSE);
+		}
+		wrefresh(win->window);
+		move = wgetch(win->window);
+
+		switch(move)
+		{
+			case 'w':
+				current--;
+				if(current==-1)
+					current=0;
+				break;
+			case 's':
+				current++;
+				if(current==MAX_LVL)
+					current=MAX_LVL-1;
+				break;
+			default:	
+				break;
+		}
+		if(move == ENTER)	break;
+	}
+
+	CleanWin(win,1);						
+	wrefresh(win->window);
+}
+
+
+
+WIN* Init(WINDOW* parent, int rows, int cols, int y, int x, int color, int bo, int delay)
+{
+	WIN* W = (WIN*)malloc(sizeof(WIN));					
+	W->x = x; W->y = y; W->rows = rows; W->cols = cols; W->color = color;
+	W->window = subwin(parent, rows, cols, y, x);
+	CleanWin(W, bo);
+	if (delay == 0)  nodelay(W->window,TRUE);							
+	// non-blocking reading of characters (for real-time game)
+	wrefresh(W->window);
+	return W;
+}
+
 
 int main()
 {
-    printf("final test :)");
-    return 0;
+	WINDOW *mainwin = Start();
+
+	Welcome(mainwin);
+
+	WIN* playwin = Init(mainwin, ROWS, COLS, OFFY, OFFX, PLAY_COLOR, BORDER, DELAY_ON);
+	Menu(playwin);
+	
+	WIN* statwin = Init(mainwin, 3, COLS, ROWS+OFFY, OFFX, STAT_COLOR, BORDER, DELAY_OFF);
+
+	getch();
+
+	endwin();
+	return 0;
 }
